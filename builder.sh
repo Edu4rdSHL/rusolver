86e760d37dc0a0509c05be725c340929f59b6441
Edu4rdSHL <edu4rdshl@protonmail.com>
2021-05-10 20:50:39 -0500

    Update builder.
    Signed-off-by: Edu4rdSHL <edu4rdshl@protonmail.com>

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@ NAME="rusolver"
 
 LINUX_TARGET="x86_64-unknown-linux-musl"
 WIN_TARGET="x86_64-pc-windows-gnu"
-RPI_TARGET="armv7-unknown-linux-gnueabihf"
+ARMV7_TARGET="armv7-unknown-linux-gnueabihf"
 AARCH_TARGET="aarch64-unknown-linux-gnu"
 OSX_TARGET="x86_64-apple-darwin"
 MANPAGE_DIR="./$NAME.1"
@@ -26,13 +26,13 @@ else
   echo "Windows artifact build: FAILED"
 fi
 
-# RPi 2-3 build
-echo "Building RPi artifact."
-if cross build -q --release --target="$RPI_TARGET"; then
-  echo "Windows artifact build: SUCCESS"
-  cp "target/$RPI_TARGET/release/$NAME" "target/$RPI_TARGET/release/$NAME-rpi"
+# ARMV7 build
+echo "Building ARMv7 artifact."
+if cross build -q --release --target="$ARMV7_TARGET"; then
+  echo "ARMv7 artifact build: SUCCESS"
+  cp "target/$ARMV7_TARGET/release/$NAME" "target/$ARMV7_TARGET/release/$NAME-armv7"
 else
-  echo "RPi artifact build: FAILED"
+  echo "ARMv7 artifact build: FAILED"
 fi
 
 # Aarch64 build
